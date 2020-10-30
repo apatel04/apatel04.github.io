792d60da722841e0852d74dc07d905c1b4833c12
apatel04 <50719234+apatel04@users.noreply.github.com>
2020-10-30 17:21:16 -0400

    changes

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,21 +1,45 @@
-import fetch from 'node-fetch';
+//const { rest, result } = require("cypress/types/lodash");
 
 function convertRestaurantsToCategories(restaurantList) {
-  // process your restaurants here!
-  return list;
+const newDataShape = restaurantList.reduce((collection, item, i) => {
+  // for each item, check if we have a category for that item already
+  const findCat = collection.find((findItem) => findItem.label === item.category);
+  
+  if (!findCat) {
+    collection.push({
+      label: item.category,
+      y: 1
+    });
+  } else {
+    //findCat.y += 1;
+    const position = collection.findIndex(el => el.label === item.category);
+    collection[position].y += 1;
+  }
+  return collection;
+}, []);
+return newDataShape;
 }
+
+
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
-  CanvasJS.addColorSet('customColorSet1', [
+
+  CanvasJS.addColorSet('greenShades', [
     // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
+    
+    "#2F4F4F",
+    "#008080",
+    "#2E8B57",
+    "#3CB371",
+    "#90EE90"  
   ]);
 
   return {
     animationEnabled: true,
-    colorSet: 'customColorSet1',
+    colorSet: 'greenShades',
     title: {
-      text: 'Change This Title'
+      text: 'Places To Eat Out In Future'
     },
     axisX: {
       interval: 1,
@@ -24,9 +48,26 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     axisY2: {
       interlacedColor: 'rgba(1,77,101,.2)',
       gridColor: 'rgba(1,77,101,.1)',
-      title: 'Change This Title',
+      title: 'Restaurants By Category',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {
+        customBreaks: [{
+            startValue: 40,
+            endValue: 50,
+            color: "pink"
+            },
+            {
+            startValue: 85,
+            endValue: 100,
+            color: "pink"
+            },
+            {
+            startValue: 140,
+            endValue: 175,
+            color: "pink"
+            }]}
+
+       // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
     data: [{
       type: 'bar',
@@ -36,6 +77,8 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     }]
   };
 }
+
+
 
 function runThisWithResultsFromServer(jsonFromServer) {
   console.log('jsonFromServer', jsonFromServer);
